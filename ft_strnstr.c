594d219e720dd67cd31d3338e23657774220873a
Katja Toivola <ktoivola@c2r3p6.hive.fi>
2023-10-30 17:43:51 +0200

    edited func

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 13:53:21 by ktoivola          #+#    #+#             */
-/*   Updated: 2023/10/30 16:51:39 by ktoivola         ###   ########.fr       */
+/*   Updated: 2023/10/30 17:32:38 by ktoivola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 	int	i;
 	int	j;
 
-	
 	if(!(*needle))
 		return((char *)haystack);
 	while (*haystack++ && len--)
 	{
 		i = 0;
 		j = len;
-		while((*(haystack + i) == needle[i]) && (j > 0) && *(haystack + i))
+		while((*(haystack + i) == needle[i]) && j-- && *(haystack + i))
 		{
 			i++;
-			j--;
 			if(needle[i] == 0)
 				return((char *)haystack);
 		}
