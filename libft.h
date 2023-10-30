ed5fe8bfb7117a33bbf31a265949002ec98921c9
Katja Toivola <ktoivola@c2r3p6.hive.fi>
2023-10-30 18:06:04 +0200

    added more functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 09:45:08 by ktoivola          #+#    #+#             */
-/*   Updated: 2023/10/30 13:51:44 by ktoivola         ###   ########.fr       */
+/*   Updated: 2023/10/30 18:03:38 by ktoivola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,27 @@
 # include <stdio.h>
 # include <string.h>
 
-int	ft_isprint(char *str);
-int	ft_isalpha(char *str);
-int	ft_isdigit(char *str);
-int	ft_isalnum(char *str);
-int	ft_isascii(char *str);
-int	ft_strlen(char *str);
+int		ft_isprint(char *str);
+int		ft_isalpha(char *str);
+int		ft_isdigit(char *str);
+int		ft_isalnum(char *str);
+int		ft_isascii(char *str);
+int		ft_strlen(char *str);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-char *ft_strchr(const char *s, int c);
-char *ft_strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char 	*ft_strchr(const char *s, int c);
+char 	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(const char *str);
 /*
-# include "ft_strnstr.c"
-# include "ft_atoi.c"
 # include "ft_calloc.c"
 # include "ft_strdup.c" */
 
