e1181870bb143ce06be933469416ade1be3980d1
Katja Toivola <ktoivola@c2r3p7.hive.fi>
2023-11-01 09:47:48 +0200

    added function and edits

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,37 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 16:53:57 by ktoivola          #+#    #+#             */
-/*   Updated: 2023/10/31 17:58:14 by ktoivola         ###   ########.fr       */
+/*   Created: 2023/10/31 17:35:59 by ktoivola          #+#    #+#             */
+/*   Updated: 2023/10/31 17:50:30 by ktoivola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	int	i;
-	int	nbr;
-
-	i = 1;
-	nbr = 0;
-	while (('\t' <= *str && *str <= '\r') || *str == ' ')
-		str++;
-	while (*str == '-' || *str == '+')
+	while (*(char *)s++ && n--)
 	{
-		if (*str == '-')
-			i *= -1;
-		str++;
+		if(*(unsigned char *)s == (unsigned char)c)
+			return((void *)s);
 	}
-	while ((*str >= '0' && *str <= '9'))
-	{
-		nbr *= 10;
-		nbr += *str - 48;
-		str++;
-	}
-	return (nbr * i);
+	return(0);
 }
