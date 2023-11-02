dd56a589630cd7af57e5bac9edb3614a7f913718
Katja Toivola <ktoivola@c2r3p7.hive.fi>
2023-11-02 14:20:21 +0200

    added substr

---
+++ ft_substr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 14:01:16 by ktoivola          #+#    #+#             */
+/*   Updated: 2023/11/02 14:19:55 by ktoivola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	i = 0;
+	substr = malloc(len * sizeof(char));
+	while (i < len && s[i])
+		substr[i] = s[start + i];
+	return(substr);
+}
