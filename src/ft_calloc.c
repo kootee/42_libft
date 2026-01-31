fa75d3e80cd551d4d0b3208c8eb35a6681e3e5c8
Katja Toivola <ktoivola@student.hive.fi>
2026-01-31 19:44:00 +0200

    adding functions

---
+++ src/ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 18:06:48 by ktoivola          #+#    #+#             */
+/*   Updated: 2023/11/21 10:44:05 by ktoivola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <stdint.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	size_t	max_size;
+
+	max_size = SIZE_MAX;
+	if (count * size > max_size)
+		return (0);
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (0);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
